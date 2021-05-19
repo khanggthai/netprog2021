de2d7d88d3419ca37a9e60a90cc8a803467d1a4e
khanggthai <75533198+khanggthai@users.noreply.github.com>
2021-05-19 10:14:10 +0700

    update labwork 6

---
+++ 06.practical.work.turn.delim/06.practical.work.client.turn.delim.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    struct hostent *hostname;
+    char *ipaddr;
+    unsigned short port = 8784;
+    char sms[50];
+    char buffer[256];
+
+    // int socket(int domain, int type, int protocol)
+    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating socket");
+        return -1;
+    }
+
+    //from labwork2
+	if(argc == 2) {
+		hostname = gethostbyname(argv[1]);
+	}
+    else {
+        printf("Please enter hostname: \n");
+        char str[50];
+        scanf("%s", str);
+        hostname = gethostbyname(str);
+	}
+
+	printf("IP address is: \n");
+
+    if (!hostname) {
+        printf("Host unknown.");
+        ipaddr = inet_ntoa(*(struct in_addr *)hostname->h_name);
+        printf(hostname->h_name, ipaddr);
+    }
+    else {
+        for (unsigned int i=0; hostname->h_addr_list[i] != NULL; i++){
+            printf("%s\n", inet_ntoa( *(struct in_addr*)(hostname->h_addr_list[i])));
+        }
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    memcpy((char *) &saddr.sin_addr.s_addr, hostname->h_addr_list[0], hostname->h_length);
+    saddr.sin_port = htons(port);
+
+    //int connect(int sockfd, const struct sockaddr *saddr, socklen_t addrlen)
+    if (connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+        printf("Cannot connect");
+        close(sockfd);
+        return -1;
+    }
+    while (1) {
+        printf("Enter the message: ");
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        int n = 0;            
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) {
+            printf("ERROR writing to socket");
+        }
+        bzero(buffer,256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0) {
+            printf("ERROR reading from socket");
+        }
+        printf("%s\n", buffer);
+        close(sockfd);
+        return 0;
+    } 
+}
---
+++ 06.practical.work.turn.delim/06.practical.work.server.turn.delim.c
@@ -0,0 +1,57 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char const *argv[]) {
+    char sms[50];
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 8784;
+    char buffer[256];
+
+    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating socket");
+        return -1;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+        printf("Error binding");
+        return -1;
+    }
+
+    if (listen(sockfd, 5) < 0) {
+        printf("Error listening");
+        return -1;
+    }
+    while (1) {
+        clen=sizeof(caddr);
+        if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+            printf("Error accepting connection\n");
+            return -1;
+        } 
+        while (1) {
+            send(clientfd, "Hello, world!%s\n", 13,0);
+            printf("Server got connected.");
+            int  n = 0;
+            n = read(clientfd,buffer,255);
+            if (n < 0){
+                printf("ERROR reading from socket");
+            }
+            printf("Client: %s\n",buffer);
+            close(clientfd);
+            close(sockfd);
+            return 0;
+        }
+    }
+}
